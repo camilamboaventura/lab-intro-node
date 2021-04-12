be617151798a3e141e4f79e45b20d7a5f91b5662
Camila Boaventura <camilamboaventura@gmail.com>
2021-04-12 23:18:26 +0200

    final version

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b) => a-b);
+    this.length ++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+     return Math.max(...this.items);
+    
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+    return Math.min(...this.items);
+  }
+ 
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } 
+   return this.items.reduce((acc, curr) => acc + curr) 
+  
+  }
 
-  sum() {}
+  
+  
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+   let sum = this.items.reduce((acc, curr) => acc + curr) ;
+   return sum/this.length
+  }
 }
 
 module.exports = SortedList;
